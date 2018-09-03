b1a9a53dba5cabe9691a96945ca4e69c847d1609
Bhumi Shahexit <pushpendrasinha@ostinohealth.com>
2018-09-03 17:53:21 +0530

    ui correction

--- client/src/app/static-content/consumer/consumer.component.ts
+++ client/src/app/static-content/consumer/consumer.component.ts
@@ -11,7 +11,7 @@ export class ConsumerComponent implements OnInit {
 
   ngOnInit() {
     // Set the date we're counting down to
-    var countDownDate = new Date("aug 16, 2018 15:37:25").getTime();
+    var countDownDate = new Date("sep 20, 2018 15:37:25").getTime();
 
 // Update the count down every 1 second
     var countdownfunction = setInterval(function() {
