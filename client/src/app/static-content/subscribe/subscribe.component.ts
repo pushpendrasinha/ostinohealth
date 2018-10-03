4e199a4df9d2fb5f75092e677a64f05c9eed1ed1
aasheesh <aasheeshbareja@gmail.com>
2018-10-03 17:09:40 +0530

    subscription & text change

--- client/src/app/static-content/subscribe/subscribe.component.ts
+++ client/src/app/static-content/subscribe/subscribe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RemoteApiService} from "../../../services/remoteapi.service";
+import { AlertHandler } from "../../../services/alert-handler";
 
 @Component({
   selector: 'app-subscribe',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.component.css']
 })
 export class SubscribeComponent implements OnInit {
-
-  constructor() { }
+  SubscriptionForm: FormGroup;
+  constructor(private fb: FormBuilder,
+              private remoteApiService: RemoteApiService,
+              private alertHandler: AlertHandler) { }
 
   ngOnInit() {
+    // this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || 'ecom/products';
+    this.SubscriptionForm = this.fb.group({
+      email: ['', Validators.required],
+      name: ['', Validators.required]
+    })
   }
+  subscribe() {
+    /*var data = {
+
+    }*/
+    this.remoteApiService.subscriptionRequest(this.SubscriptionForm.value).subscribe((result: any) => {
+      if(result.success) {
+        this.alertHandler.SuccessAlert(result.msg)
+      } else {
+        this.alertHandler.ErrorAlert(result.msg);
+      }
+    }, (err) => {
+
+    })
+  }
+
+
+
 
 }
